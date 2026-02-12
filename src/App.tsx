dd7d41968862911c3f593e394037612b9a54a6a6
victorsalvador3-tech <victorsalvador3@gmail.com>
2026-02-12 22:25:47 +0100

    Create src/App.tsx file for main application component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Savlamoto Platform</h1>
+            <p>This is the main application component for the Savlamoto platform.</p>
+        </div>
+    );
+};
+
+export default App;
